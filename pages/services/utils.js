f392f301f7b8ecac1fd81d75284ea78c546f4960
isakbet <yosephbet@gmail.com>
2021-08-11 07:28:43 +0200

    Make it much better

--- pages/services/utils.js
+++ pages/services/utils.js
@@ -1,6 +1,6 @@
 import { convertTime, kmToM, mpsToMph, timeToAMPM } from "./converters";
 
-export const isPM = (time) => {
+export const isPM = time => {
     let hours = time.split(":")[0];
     if (hours >= 12) {
         return "PM";
@@ -9,19 +9,11 @@ export const isPM = (time) => {
     }
 };
 
-export const getWindSpeed = (systemUsed, windInMph) =>
-    systemUsed == "metric" ? windInMph : mpsToMph(windInMph);
+export const getWindSpeed = (systemUsed, windInMph) => systemUsed == "metric" ? windInMph : mpsToMph(windInMph);
 
-export const getVisibility = (systemUsed, visibilityInKm) =>
-    systemUsed == "metric"
-        ? (visibilityInKm / 1000).toPrecision(2)
-        : kmToM(visibilityInKm / 1000);
+export const getVisibility = (systemUsed, visibilityInKm) => systemUsed == "metric" ? (visibilityInKm / 1000).toPrecision(2) : kmToM(visibilityInKm / 1000);
 
-export const getTime = (systemUsed, currentTime, timezone) =>
-    systemUsed == "metric"
-        ? `${parseInt(convertTime(currentTime, timezone)[0].split(":")[0])}:${convertTime(currentTime, timezone)[0].split(":")[1]
-        }`
-        : timeToAMPM(convertTime(currentTime, timezone)[0]);
+export const getTime = (systemUsed, currentTime, timezone) => systemUsed == "metric" ? `${parseInt(convertTime(currentTime, timezone)[0].split(":")[0])}:${convertTime(currentTime, timezone)[0].split(":")[1]}` : timeToAMPM(convertTime(currentTime, timezone)[0]);
 
-export const getAMPM = (systemUsed, currentTime, timezone) =>
-    systemUsed == "imperial" ? isPM(convertTime(currentTime, timezone)[0]) : "";
+export const getAMPM = (systemUsed, currentTime, timezone) => systemUsed == "imperial" ? isPM(convertTime(currentTime, timezone)[0]) : "";
+
