3c0a10be175a1ae01877c70bec59d1ef9154628c
isakbet <yosephbet@gmail.com>
2021-08-11 07:46:41 +0200

    Make it much better

--- pages/services/converters.js
+++ pages/services/converters.js
@@ -1,26 +1,92 @@
-export const ctoF = c => c * 9 / 5 + 32;
+"use strict";
 
-export const mpsToMph = mps => (mps * 2.236936).toFixed(2);
+exports.__esModule = true;
+exports.convertTime =
+    exports.degToCompass =
+    exports.timeToAMPM =
+    exports.kmToM =
+    exports.mpsToMph =
+    exports.ctoF =
+    void 0;
 
-export const kmToM = km => (km / 1.609).toFixed(1);
+var _this = void 0;
 
-export const timeToAMPM = time => {
-    let hours = time.split(":")[0];
-    let minutes = time.split(":")[1];
+function _newArrowCheck(innerThis, boundThis) {
+    if (innerThis !== boundThis) {
+        throw new TypeError("Cannot instantiate an arrow function");
+    }
+}
+
+var ctoF = function ctoF(c) {
+    _newArrowCheck(this, _this);
+
+    return (c * 9) / 5 + 32;
+}.bind(void 0);
+
+exports.ctoF = ctoF;
+
+var mpsToMph = function mpsToMph(mps) {
+    _newArrowCheck(this, _this);
+
+    return (mps * 2.236936).toFixed(2);
+}.bind(void 0);
+
+exports.mpsToMph = mpsToMph;
+
+var kmToM = function kmToM(km) {
+    _newArrowCheck(this, _this);
+
+    return (km / 1.609).toFixed(1);
+}.bind(void 0);
+
+exports.kmToM = kmToM;
+
+var timeToAMPM = function timeToAMPM(time) {
+    _newArrowCheck(this, _this);
+
+    var hours = time.split(":")[0];
+    var minutes = time.split(":")[1];
     hours = hours % 12;
     hours = hours ? hours : 12;
     return hours + ":" + minutes;
-};
+}.bind(void 0);
 
-export const degToCompass = num => {
+exports.timeToAMPM = timeToAMPM;
+
+var degToCompass = function degToCompass(num) {
+    _newArrowCheck(this, _this);
+
     var val = Math.floor(num / 22.5 + 0.5);
-    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "S/SE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    var arr = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "S/SE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW"
+    ];
     return arr[val % 16];
-};
+}.bind(void 0);
 
-export const convertTime = (unixSeconds, timezone) => {
-    const time = new Date((unixSeconds + timezone) * 1000).toISOString().match(/(\d{2}:\d{2})/);
+exports.degToCompass = degToCompass;
 
+var convertTime = function convertTime(unixSeconds, timezone) {
+    _newArrowCheck(this, _this);
+
+    var time = new Date((unixSeconds + timezone) * 1000)
+        .toISOString()
+        .match(/(\d{2}:\d{2})/);
     return time;
-};
+}.bind(void 0);
 
+exports.convertTime = convertTime;
--- pages/services/utils.js
+++ pages/services/utils.js
@@ -1,19 +1,79 @@
-import { convertTime, kmToM, mpsToMph, timeToAMPM } from "./converters";
+"use strict";
 
-export const isPM = time => {
-    let hours = time.split(":")[0];
+exports.__esModule = true;
+exports.getAMPM =
+    exports.getTime =
+    exports.getVisibility =
+    exports.getWindSpeed =
+    exports.isPM =
+    void 0;
+
+var _converters = require("./converters");
+
+var _this = void 0;
+
+function _newArrowCheck(innerThis, boundThis) {
+    if (innerThis !== boundThis) {
+        throw new TypeError("Cannot instantiate an arrow function");
+    }
+}
+
+var isPM = function isPM(time) {
+    _newArrowCheck(this, _this);
+
+    var hours = time.split(":")[0];
+
     if (hours >= 12) {
         return "PM";
     } else {
         return "AM";
     }
-};
+}.bind(void 0);
 
-export const getWindSpeed = (systemUsed, windInMph) => systemUsed == "metric" ? windInMph : mpsToMph(windInMph);
+exports.isPM = isPM;
 
-export const getVisibility = (systemUsed, visibilityInKm) => systemUsed == "metric" ? (visibilityInKm / 1000).toPrecision(2) : kmToM(visibilityInKm / 1000);
+var getWindSpeed = function getWindSpeed(systemUsed, windInMph) {
+    _newArrowCheck(this, _this);
 
-export const getTime = (systemUsed, currentTime, timezone) => systemUsed == "metric" ? `${parseInt(convertTime(currentTime, timezone)[0].split(":")[0])}:${convertTime(currentTime, timezone)[0].split(":")[1]}` : timeToAMPM(convertTime(currentTime, timezone)[0]);
+    return systemUsed == "metric"
+        ? windInMph
+        : (0, _converters.mpsToMph)(windInMph);
+}.bind(void 0);
 
-export const getAMPM = (systemUsed, currentTime, timezone) => systemUsed == "imperial" ? isPM(convertTime(currentTime, timezone)[0]) : "";
+exports.getWindSpeed = getWindSpeed;
 
+var getVisibility = function getVisibility(systemUsed, visibilityInKm) {
+    _newArrowCheck(this, _this);
+
+    return systemUsed == "metric"
+        ? (visibilityInKm / 1000).toPrecision(2)
+        : (0, _converters.kmToM)(visibilityInKm / 1000);
+}.bind(void 0);
+
+exports.getVisibility = getVisibility;
+
+var getTime = function getTime(systemUsed, currentTime, timezone) {
+    _newArrowCheck(this, _this);
+
+    return systemUsed == "metric"
+        ? parseInt(
+            (0, _converters.convertTime)(currentTime, timezone)[0].split(":")[0]
+        ) +
+        ":" +
+        (0, _converters.convertTime)(currentTime, timezone)[0].split(":")[1]
+        : (0, _converters.timeToAMPM)(
+            (0, _converters.convertTime)(currentTime, timezone)[0]
+        );
+}.bind(void 0);
+
+exports.getTime = getTime;
+
+var getAMPM = function getAMPM(systemUsed, currentTime, timezone) {
+    _newArrowCheck(this, _this);
+
+    return systemUsed == "imperial"
+        ? isPM((0, _converters.convertTime)(currentTime, timezone)[0])
+        : "";
+}.bind(void 0);
+
+exports.getAMPM = getAMPM;
