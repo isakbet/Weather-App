71572917a520a743742bb16e1e5d8f96e82cd8d8
isakbet <yosephbet@gmail.com>
2022-06-27 15:50:53 +0200

    Add style to geolocation, searchbox, weather

---
+++ src/components/styles/StyledSearchBox.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const StyledSearchBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 15px 15px;
+  & input {
+    width: 100%;
+    max-width: 280px;
+    padding: 10px 15px;
+    border: none;
+    outline: none;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 16px 0px 16px 0px;
+    border-bottom: 3px solid rgba(0, 0, 0, 0.3);
+    color: #313131;
+    font-size: 20px;
+    transition: all 0.3s ease-out;
+    &:focus {
+      background-color: rgba(255, 255, 255, 0.6);
+      border-color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  & button {
+    background-color: #fff;
+    border: none;
+    outline: none;
+    border-radius: 0px 16px 0px 16px;
+    padding: 10px 15px;
+    font-size: 20px;
+    color: #313131;
+    transition: all 0.3s ease-out;
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.6);
+      color: #313131;
+    }
+  }
+`;
+
+export default StyledSearchBox;
