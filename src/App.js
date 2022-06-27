7a3481ccef4334a33b8dcac1d9067a7fdf74a662
isakbet <yosephbet@gmail.com>
2022-06-27 13:23:34 +0200

    Remove old code, import Weather.js from components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import './App.css';
-import Forecast from "./components/Forecast/Forecast";
-import Logo from './components/Logo/Logo';
+import React from "react";
+
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo />
-        <h1>React Weather App</h1>
-      </header>
-      <main>
-        <Forecast />
-      </main>
-      <footer className='footer'>
-         <p>&copy; 2022 - <a href="yoseph.dev">Yoseph Berhane</a>. All rights reserved.</p>       
-      </footer>
+      <Weather />
     </div>
   );
 }
