41bbe675f38ff8b29e1be914a6c1455bdafb036a
isakbet <yosephbet@gmail.com>
2022-06-27 15:47:20 +0200

    Add geolocation.js

---
+++ src/components/Geolocation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import StyledGeolocation from "./styles/StyledGeolocation";
+
+const Geolocation = ({ location, currentDate }) => (
+  <StyledGeolocation>
+    {/* <h1>{location.city}</h1> */}
+    <h1> {location.city} </h1>
+    <h1>
+      {" "}
+      {location.state}, {location.country}{" "}
+    </h1>
+    <h3> {currentDate} </h3>
+    {/* <h3> {location.timezone} </h3> */}
+  </StyledGeolocation>
+);
+
+export default Geolocation;
